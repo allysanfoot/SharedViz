1e045ff56a104a40939d644927057436ac4d8f48
allysanfoot <allysafoot@gmail.com>
2024-10-16 10:13:01 -0700

    feat: building video search results

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Import components
 import { Button } from 'semantic-ui-react';
+import VideoList from './VideoList';
 
 const SearchResults = ({ addVideoToQueue, loading, page, playSearchedVideo, search, searchInput, searchResults }) => {
     const handlePreviousPage = () => {
@@ -14,7 +15,12 @@ const SearchResults = ({ addVideoToQueue, loading, page, playSearchedVideo, sear
 
     return (
         //TODO: List search results
-        <div className='navigation'>
+        <div>
+            <VideoList
+                onVideoPlay={selectedVideo => playSearchedVideo(selectedVideo)}
+                onVideoAddToQueue={selectedVideo => addVideoToQueue(selectedVideo)}
+                searchResults={searchResults}
+            />
             <Button.Group>
                 <Button onClick={handlePreviousPage} disabled={page - 1 < 1}>Prev</Button>
                 <Button.Or text={page} />
